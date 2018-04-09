641346ca2c0d4f5e7eba78cb03d5909b6a10b84a
krsktilos <krsktilos@sm-komandor.ru>
2018-04-09 12:07:26 +0700

    улучшена стабилизация веса; добавлена установка тары

--- Commander.ino
+++ Commander.ino
@@ -14,14 +14,25 @@ Q2HX711 scale(7,6);
 /* Допустимое отклонение в граммах */
 #define THRESHOLD 3;
 
-/* Чистое значение давления в 500 грамм */
-long STANDART500VALUE = 8776400;
+/* Тестовая калибровка */
+#define DEBUG_CALIBRATIONVALUE 8776400;
+#define DEBUG_CALIBRATIONMASS 500.0;
+#define DEBUG_ZEROVALUE 8663400;
+
+/* Релизная калибровка */
+#define PROD_CALIBRATIONVALUE 8875700;
+#define PROD_CALIBRATIONMASS 1000.0;
+#define PROD_ZEROVALUE 8805300;
+
+/* Чистое значение с внешним давлением */
+long CALIBRATIONVALUE = DEBUG_CALIBRATIONVALUE;
 /* Стабилизационный вес в граммах */
-float STANDART500 = 500.0;
+float CALIBRATIONMASS = DEBUG_CALIBRATIONMASS;
 /* Чистое значение без внешнего давления */
-long ZEROVALUE = 8663400;
+long ZEROVALUE = DEBUG_ZEROVALUE;
 /* Коэффициент масштабирования веса */
 float koef = 1.0;
+
 /* ВКЛ/ВЫКЛ отладочного вывода */
 bool DEBUG = false;
 
@@ -30,7 +41,7 @@ long stack[STACK_SIZE];
 long tmp[STACK_SIZE];
 
 bool stabilized = true;
-long weight = 0;
+long average = 0;
 long gain = 0;
 
 void print(String message) {if (DEBUG) {Serial.print(message);}}
@@ -47,17 +58,18 @@ void setup() {
   print("koef:");
   println(String(koef));
   println("clear stack...");
-  clearStack();
+  stackClear();
   println("find zero value...");
   ZEROVALUE = getRawValue();
   println(String(ZEROVALUE));
-  gain = calcGain();
+  prepare();
+  setGain();
   print("gain:");
   println(String(gain));
   println(">>> DONE <<<");
 }
 
-void clearStack() {
+void stackClear() {
   for (int i=0; i<STACK_SIZE; i++) {
     stack[i] = 0;
     tmp[i] = 0;
@@ -78,20 +90,23 @@ void stackPush(long value) {
  * Коэффициент масштабирования.
  */
 float calcKoef() {
-  return (STANDART500VALUE - ZEROVALUE) / STANDART500;
+  return (CALIBRATIONVALUE - ZEROVALUE) / CALIBRATIONMASS;
+}
+
+void prepare() {
+  println("init...");
+  for (int i=0; i<STACK_SIZE*2; i++) {
+    getValue();
+    delay(LOOP_DELAY);
+  }
 }
 
 /**
  * Иинициализации утяжеления.
  */
-long calcGain() {
-  println("calculate gain...");
-  long tmp = 0;
-  for (int i=0; i<20; i++) {
-    tmp = getValue();
-    delay(100);
-  }
-  return tmp;
+void setGain() {
+  println("set gain...");
+  gain += getValue();
 }
 
 /**
@@ -107,7 +122,10 @@ long getValue() {
   print(String(w));
   print(";");
   print("stable:");
-  println(String(stabilized));
+  print(String(stabilized));
+  print(";");
+  print("gain:");
+  println(String(gain));
   return w;
 }
 
@@ -123,7 +141,7 @@ long getRawValue() {
  * Определяет текущее состояние весов.
  */
 void checkStatus() {
-  long average = 0;
+  average = 0;
   for (int i=0; i<STACK_SIZE; i++) {
     average += stack[i];
   }
@@ -146,20 +164,29 @@ void checkStatus() {
 void receiveCommands() {
   if (Serial.available() > 0) {
     char b = Serial.read();
-    if (b == ':') {
-      byte buf[4];
-      buf[0] = weight & 255;
-      buf[1] = (weight >> 8)  & 255;
-      buf[2] = (weight >> 16) & 255;
-      buf[3] = (weight >> 24) & 255;
-      Serial.write(buf, 4);
-      Serial.write(stabilized ? 1 : 0);
+    switch (b) {
+      case ':': {
+        /* Передаем текущее состояние */
+        byte buf[5];
+        buf[0] = average & 255;
+        buf[1] = (average >> 8)  & 255;
+        buf[2] = (average >> 16) & 255;
+        buf[3] = (average >> 24) & 255;
+        buf[4] = stabilized ? 1 : 0;
+        Serial.write(buf, 5);
+        break;
+      }
+      case ';': {
+        /* Сброс в ноль */
+        setGain();
+        break;
+      }
     }
   }
 }
 
 void step() {
-  weight = getValue();
+  long weight = getValue();
   stackPush(weight);
 }
 
